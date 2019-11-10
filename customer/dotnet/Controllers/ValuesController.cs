85be9cb73fc669dfc15ab39542293dcc5a560907
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2019-11-11 01:41:46 +1100

    add support for v3 customer

--- customer/dotnet/Controllers/ValuesController.cs
+++ customer/dotnet/Controllers/ValuesController.cs
@@ -1,23 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Net.Http;
 
 namespace dotnet.Controllers
 {
     [Route("/")]
     public class ValuesController : Controller
     {
-        const string url = "http://preference:8080";
-        const string responseStringFormat = "customer => {0}\n";
+        const string url = "http://recommendation:8080/";
+        const string responseStringFormat = "Customer {0} => {1}\n";
+
+        static HttpClient client = new HttpClient();
+
+        private string callPreference()
+        {
+            string response = "unknown";
+            using (var task = client.GetStringAsync(url))
+            {
+                response = task.Result;
+            }
+
+            return response;
+        }
+
         // GET api/values
         [HttpGet]
-        public string Get()
+        public IActionResult Get()
         {
+            HttpStatusCode code = HttpStatusCode.OK;
+
+            var messageOverride = System.Environment.GetEnvironmentVariable("MESSAGE_OVERRIDE");
+            Console.WriteLine("MESSAGE_OVERRIDE is {0}", messageOverride == null ? "not existent" : messageOverride);
+
             string hostname = Dns.GetHostName();
-            return String.Format(responseStringFormat, hostname);
+            string preferenceResponse = "<unknown>";
+            if (messageOverride != null)
+            {
+                preferenceResponse = messageOverride;
+            }
+            else
+            {
+                try
+                {
+                    preferenceResponse = callPreference();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error calling preference service: {0}", e.Message);
+                    code = HttpStatusCode.ServiceUnavailable;
+                }
+            }
+
+            return StatusCode((int)code, String.Format(responseStringFormat, hostname, preferenceResponse));
         }
     }
 }
