ceb976d2abaaa19ad5c71a5dd947dc5babaa5d6d
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-01-27 16:27:11 +1100

    Revert "Squashed commit of the following:"
    This reverts commit 34daee7f2b6e3d12c3a8fde4ece76ad56bae81aa.

--- scripts/01-provision-service-mesh.sh
+++ scripts/01-provision-service-mesh.sh
@@ -13,18 +13,18 @@ oc apply -f ${SCRIPT_DIR}/service-mesh.yaml -n istio-system
 # define which projects will participate in the service mesh (and thus will have sidecar injected into them)
 oc apply -f ${SCRIPT_DIR}/service-mesh-roll.yaml -n istio-system
 
-printf "Move on to the next script once all the following pods have been created:
-grafana-b67df64b6-dwx44                
-istio-citadel-79979464d-qh6fn          
-istio-egressgateway-7d897695c4-p6jbj   
-istio-galley-6bb46858c5-k6z2w          
-istio-ingressgateway-8465bbf788-phcjn  
-istio-pilot-54b65495c4-ks46p           
-istio-policy-5fc74b8697-cldbw          
-istio-sidecar-injector-65cd4c8c6f-8nq4k
-istio-telemetry-69cb778b9-j4xhw        
-jaeger-57776787bc-j8rq8                
-kiali-6d6f9cf658-sthwl                 
-prometheus-b8bdc6b77-kpgtn             "
+printf "Move on to the next script once all the following pods have been created:\n
+grafana-b67df64b6-dwx44                \n
+istio-citadel-79979464d-qh6fn          \n
+istio-egressgateway-7d897695c4-p6jbj   \n
+istio-galley-6bb46858c5-k6z2w          \n
+istio-ingressgateway-8465bbf788-phcjn  \n
+istio-pilot-54b65495c4-ks46p           \n
+istio-policy-5fc74b8697-cldbw          \n
+istio-sidecar-injector-65cd4c8c6f-8nq4k\n
+istio-telemetry-69cb778b9-j4xhw        \n
+jaeger-57776787bc-j8rq8                \n
+kiali-6d6f9cf658-sthwl                 \n
+prometheus-b8bdc6b77-kpgtn             \n"
 
 oc get pods -n istio-system -w
