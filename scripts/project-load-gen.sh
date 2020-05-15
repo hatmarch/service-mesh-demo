8ea970f8417ba1fb600e4678f423e4e600e4df08
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-05-15 13:32:10 +0000

    Fix loadgen script to work with SM 1.1.1
    Add more explicit flags for use (whether to call through to istio or to address the service directly)

--- scripts/project-load-gen.sh
+++ scripts/project-load-gen.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
-PROJECT="demo-app"
+set -e -u -o pipefail
+declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
+declare -r DEMO_HOME="$SCRIPT_DIR/.."
+declare PROJECT_NAME="demo-app"
+declare ROUTE=""
+declare ISTIO_ROUTE=""
 
-ROUTE=$1
+while (( "$#" )); do
+    case "$1" in
+        -p|--project)
+            PROJECT_NAME=$2
+            shift 2
+            ;;
+        --istio)
+            ISTIO_ROUTE="true"
+            shift
+            ;;
+        -*|--*)
+            echo "Error: Unsupported flag $1"
+            exit 1
+            ;;
+        *) 
+            ROUTE=$1
+            shift
+            ;;
+    esac
+done
+
+declare -r ISTIO_PRJ="${PROJECT_NAME}-istio-system"
+
 if [ -z "$ROUTE" ]; then
     echo "Must specify the *short name* of a route (and optionally whether to use istio gateway.)"
     exit 1
 fi
 
 
-if [ "$2" == 'istio' ]; then
-    URL="$(oc -n istio-system get route istio-ingressgateway -o jsonpath='{.spec.host}')/$ROUTE"
+if [ "$ISTIO_ROUTE" ]; then
+    URL="$(oc -n ${ISTIO_PRJ} get route istio-ingressgateway -o jsonpath='{.spec.host}')/$ROUTE"
 else
-    URL="$(oc -n $PROJECT get route $ROUTE -o jsonpath='{.spec.host}')"
+    URL="$(oc -n ${PROJECT_NAME} get route $ROUTE -o jsonpath='{.spec.host}')"
 fi
 
 read -n 1 -p "Continuous load gen for $URL?  Press Y to proceed and N for single call (y/N)" COMPLETE
