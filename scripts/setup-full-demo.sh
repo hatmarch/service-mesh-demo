6f39c60fda117d1e9110bd64887e86ae8a9c876f
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-05-15 13:30:34 +0000

    One script demo setup
    Update readme with helpful istio tips

---
+++ scripts/setup-full-demo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Fully setups the demo on the cluster
+
+set -e -u -o pipefail
+declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
+declare -r DEMO_HOME="$SCRIPT_DIR/.."
+declare PROJECT_NAME="demo-app"
+declare REMOVE_OPERATORS="true"
+
+while (( "$#" )); do
+    case "$1" in
+        -p|--project)
+            PROJECT_NAME=$2
+            shift 2
+            ;;
+        -k|--keep-operators)
+            REMOVE_OPERATORS=""
+            shift 1
+            ;;
+        -*|--*)
+            echo "Error: Unsupported flag $1"
+            exit 1
+            ;;
+        *) 
+            break
+    esac
+done
+
+$SCRIPT_DIR/00-service-mesh-cluster-install.sh -p $PROJECT_NAME
+$SCRIPT_DIR/01-provision-service-mesh.sh -p $PROJECT_NAME
+$SCRIPT_DIR/02-project-no-service-mesh-setup.sh -p $PROJECT_NAME
+$SCRIPT_DIR/03-project-apply-service-mesh.sh -p $PROJECT_NAME
+
+echo "Done!"
