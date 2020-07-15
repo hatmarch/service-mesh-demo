81549f971fe92a1c925a313327e14f8fd87e74a8
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-07-15 11:24:34 +0000

    Add option to force delete projects if they get stuck

--- scripts/remove-demo.sh
+++ scripts/remove-demo.sh
@@ -6,12 +6,17 @@ declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
 declare -r DEMO_HOME="$SCRIPT_DIR/.."
 declare PROJECT_NAME="demo-app"
 declare REMOVE_OPERATORS="true"
+declare FORCE=""
 
 while (( "$#" )); do
     case "$1" in
         -p|--project)
             PROJECT_NAME=$2
             shift 2
+            ;;
+        -f|--force)
+            FORCE="true"
+            shift 1
             ;;
         -k|--keep-operators)
             REMOVE_OPERATORS=""
@@ -25,6 +30,17 @@ while (( "$#" )); do
             break
     esac
 done
+
+# Assumes proxy has been setup
+force-clean() {
+    declare NAMESPACE=$1
+
+    echo "Force removing project $NAMESPACE"
+
+    oc get namespace $NAMESPACE -o json | jq '.spec = {"finalizers":[]}' > /tmp/temp.json
+    curl -k -H "Content-Type: application/json" -X PUT --data-binary @/tmp/temp.json 127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
+    rm /tmp/temp.json
+}
 
 declare ISTIO_PRJ="${PROJECT_NAME}-istio-system"
 
@@ -43,9 +59,22 @@ if [[ "${REMOVE_OPERATORS}" ]]; then
     done
 fi
 
+declare PROXY_PID=""
+if [[ ! -z "$FORCE" ]]; then
+    echo -n "opening proxy"
+
+    oc proxy &
+    PROXY_PID=$!
+fi
+
 # wait until all projects are fully deleted
 for PROJECT in "${PROJECTS[@]}"; do
     while [[ "$(oc get ns ${PROJECT} 2>/dev/null)" ]]; do
+
+        if [[ ! -z "$FORCE" ]]; then
+            force-clean "${PROJECT}"
+        fi
+
         echo "Waiting for ${PROJECT} deletion."
         sleep 1
     done
@@ -54,4 +83,8 @@ done
 # FIXME: Wait until all the operators are removed
 
 
+if [[ ! -z "$PROXY_PID" ]]; then
+    echo "closing proxy"
+    kill $PROXY_PID
+fi
 
