b1e03d9b149f41badb6375c9e10b93b3efcb6abb
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-09-10 08:36:47 +0000

    Update setup for dealing with AWS clusters

--- scripts/shell-setup.sh
+++ scripts/shell-setup.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
+declare SCRIPT=$0
+if [[ "$SCRIPT" == "/bin/bash" ]]; then
+    SCRIPT="${BASH_SOURCE}"
+fi
 
-export DEMO_HOME=`pwd`
+if [[ -z "${SCRIPT}" ]]; then
+    echo "BASH_SOURCE: ${BASH_SOURCE}, 0 is: $0"
+    echo "Failed to find the running name of the script, you need to set DEMO_HOME manually"
+fi
+
+export DEMO_HOME=$( cd "$(dirname "${SCRIPT}")/.." ; pwd -P )
+
+
+aws-up() {
+    local CLUSTER_NAME=${1:-${CLUSTERNAME}}
+    if [[ -z "${CLUSTER_NAME}" ]]; then
+        echo "Must provide a cluster name either as parameter or in environment variable `CLUSTERNAME`"
+        return 1
+    fi
+
+    local AWS_REGION=${REGION}
+    if [[ -z "${AWS_REGION}" ]]; then
+        echo "Must provide a region by way of REGION environment variable"
+        return 1
+    fi
+
+    aws ec2 start-instances --instance-ids --region=${AWS_REGION} \
+        $(aws ec2 describe-instances --region ${AWS_REGION} --query 'Reservations[*].Instances[*].{Instance:InstanceId}' --output text --filters "Name=tag-key,Values=kubernetes.io/cluster/${CLUSTER_NAME}-*" "Name=instance-state-name,Values=stopped")
+}
+
+aws-down() {
+    local CLUSTER_NAME=${1:-${CLUSTERNAME}}
+    if [[ -z "$CLUSTER_NAME" ]]; then
+        echo "Must provide a cluster name either as parameter or in environment variable `CLUSTERNAME`"
+        return 1
+    fi
+
+    local AWS_REGION=${REGION}
+    if [[ -z "${AWS_REGION}" ]]; then
+        echo "Must provide a region by way of REGION environment variable"
+        return 1
+    fi
+
+    aws ec2 stop-instances --instance-ids --region ${AWS_REGION} \
+        $(aws ec2 describe-instances --region ${AWS_REGION} --query 'Reservations[*].Instances[*].{Instance:InstanceId}' --output text --filters "Name=tag-key,Values=kubernetes.io/cluster/${CLUSTER_NAME}-*" "Name=instance-state-name,Values=running") 
+}
+
 alias cds="cd ${DEMO_HOME}/scripts"
 alias cdh='cd ${DEMO_HOME}'
-# oc login -u $USER -p $PASSWORD --server=$SERVER
